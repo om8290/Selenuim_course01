76f6f04e21473a36b51a9f6465933e7da88dfc9c
om8290 <om.rastogi@tothenew.com>
2025-03-02 23:40:58 +0530

    this is the basic

--- src/main/java/org/example/DropStatic.java
+++ src/main/java/org/example/DropStatic.java
@@ -6,8 +6,10 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 
 import java.time.Duration;
+import java.util.List;
 
 
 public class DropStatic {
@@ -17,7 +19,7 @@ public class DropStatic {
          web1.get("https://rahulshettyacademy.com/dropdownsPractise/");
          web1.getTitle();
          web1.manage().window().maximize();
-
+// web1.findElement(By.xpath("//input[@id='ctl00_mainContent_rbtnl_Trip_1']")).click();
 
 
         WebElement staticdrop= web1.findElement(By.id("ctl00_mainContent_DropDownListCurrency"));
@@ -39,7 +41,30 @@ public class DropStatic {
 web1.findElement(By.id("ctl00_mainContent_ddl_originStation1_CTXT")).click();
         web1.findElement(By.xpath("//a[@value='BKK']")).click();
         Thread.sleep(2000L);
-        web1.findElement(By.xpath("(//a[@value='MAA'])[2]")).click();
+//        web1.findElement(By.xpath("(//a[@value='MAA'])[2]")).click();
+
+        web1.findElement(By.xpath("//div[@id='ctl00_mainContent_ddl_destinationStation1_CTNR']//a[@value='MAA']")).click();
+
+web1.findElement(By.id("autosuggest")).sendKeys("ind");
+
+Thread.sleep(3000L);
+        List<WebElement> options = web1.findElements(By.cssSelector("li.ui-menu-item a"));
+        Thread.sleep(3000L);
+for(WebElement op: options){
+     if(op.getText().equalsIgnoreCase("India")){
+          op.click();
+          break;
+     }
+}
+        web1.findElement(By.xpath("//input[@id='ctl00_mainContent_chk_friendsandfamily']")).click();
+ web1.findElement(By.cssSelector(".ui-state-default.ui-state-active")).click();
+  Thread.sleep(3000L);
+web1.findElement(By.xpath("//input[@type='submit']")).click();
+
+        Thread.sleep(3000L);
+
+
+         web1.quit();
 // json wire protocaol
 
 
